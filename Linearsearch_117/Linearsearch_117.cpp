a9a02b98192af6f00a3708c1d329ba227ba8f4ba
nauvalachid <nauvalachid@gmail.com>
2023-04-13 14:40:29 +0700

    Menerima element array

--- Linearsearch_117/Linearsearch_117.cpp
+++ Linearsearch_117/Linearsearch_117.cpp
@@ -4,3 +4,16 @@ using namespace std;
 int arr[20];
 int n;
 int i;
+
+
+void input()
+{
+	while (true)
+	{
+		cout << "Masukkan jumlah elemen dalam array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray harus memiliki minimal 1 dan maksimal 20 elemen.\n\n";
+	}
