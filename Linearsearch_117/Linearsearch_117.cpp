81540b4903450aeb9a3b23dc090ecdbd9377be67
nauvalachid <nauvalachid@gmail.com>
2023-04-13 14:44:04 +0700

    membuat method void input

--- Linearsearch_117/Linearsearch_117.cpp
+++ Linearsearch_117/Linearsearch_117.cpp
@@ -17,3 +17,14 @@ void input()
 		else
 			cout << "\nArray harus memiliki minimal 1 dan maksimal 20 elemen.\n\n";
 	}
+
+
+	cout << "\n-------------------\n";
+	cout << " memasukkan elemen array \n";
+	cout << "---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
+}
